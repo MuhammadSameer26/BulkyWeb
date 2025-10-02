fe0f49b4f1bf71ccfc71f96f9a714e573605a928
Kaleem Shahzad <kaleemshahzad131@gmail.com>
2025-10-03 04:25:58 +0500

    crud first commit

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BulkyWeb.Controllers
 
         public IActionResult Index()
         {
-            return View("privacy");
+            return View();
         }
 
         public IActionResult Privacy()
