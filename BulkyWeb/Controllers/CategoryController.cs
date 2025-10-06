1bf0887eaa4a354df4194e0f4a5b541b3cc1a878
Kaleem Shahzad <kaleemshahzad131@gmail.com>
2025-10-06 06:04:55 +0500

    update almost completed

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.data;
 using Microsoft.AspNetCore.Mvc;
 using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
@@ -22,5 +23,44 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+               ModelState.AddModelError("name", "The Display order cannot exactly match the Name.");
+            }
+            
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==0 || id == null)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            return View(CategoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
     public class Category
     {
         [Key]
+        
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Name Field is Required")]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Name")]
+        [Range(1,100,ErrorMessage ="Display order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
