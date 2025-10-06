7de74b9acc05ce28386215ec4c872bb33acd83c4
Muhammadsameer23 <sameershahzad655@gmail.com>
2025-10-06 18:29:51 +0500

    changes

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -20,7 +20,17 @@ namespace BulkyWeb.Controllers
 
         public IActionResult Create()
         {
-            return View();
+
+           return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
